de03f3b3378914edf6ee71e0bc64e93c3046bfef
Eisfuchs <Eisfuchs@gmail.com>
2014-10-01 20:38:00 +0200

    #6

---
+++ src/Views/MemberView.java
@@ -0,0 +1,17 @@
+package Views;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class MemberView extends VerticalLayout implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		addComponent(new Label("Mitgliederverwaltung"));
+		
+	}
+
+}
