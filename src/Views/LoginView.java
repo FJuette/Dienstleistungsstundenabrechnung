d9c0e2cd69e645dbcd75becc51cbc229111d1700
Eisfuchs <Eisfuchs@gmail.com>
2014-10-01 20:36:57 +0200

    #11

---
+++ src/Views/LoginView.java
@@ -0,0 +1,16 @@
+package Views;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class LoginView extends VerticalLayout implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
