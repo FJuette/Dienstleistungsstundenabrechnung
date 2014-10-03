e2b577a763429718c0dd583c4062104e728fa20f
Eisfuchs <Eisfuchs@gmail.com>
2014-10-03 20:22:15 +0200

    #9 Aufräumen

--- src/Views/GroupsView.java
+++ src/Views/GroupsView.java
@@ -2,32 +2,20 @@ package Views;
 
 import model.Group;
 
-
-
-import com.vaadin.data.Container;
-import com.vaadin.data.Item;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.fieldgroup.PropertyId;
-import com.vaadin.data.util.BeanItem;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
-
-
 
 import de.juette.dlsa.BooleanToGermanConverter;
 
@@ -58,7 +46,7 @@ public class GroupsView extends HorizontalSplitPanel implements View {
 		tblGroups.setContainerDataSource(groups);
 		tblGroups.setSelectable(true);
 		tblGroups.setVisibleColumns( new Object[] {"gruppenname", "befreit"} );
-		tblGroups.setColumnHeaders("Gruppenname", "Befreit");
+		tblGroups.setColumnHeaders("Gruppe", "Befreit");
 		tblGroups.setHeight("400");
 		tblGroups.setConverter("befreit", new BooleanToGermanConverter());
 		
