387a16567dbd86c06edc87975fdb1c852e988abf
Eisfuchs <Eisfuchs@gmail.com>
2014-10-03 20:23:37 +0200

    #11 Erste Implementierungen

--- src/Views/LoginView.java
+++ src/Views/LoginView.java
@@ -1,8 +1,19 @@
 package Views;
 
+//import org.apache.shiro.SecurityUtils;
+//import org.apache.shiro.authc.UsernamePasswordToken;
+//import org.apache.shiro.subject.Subject;
+
+import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 @SuppressWarnings("serial")
 public class LoginView extends VerticalLayout implements View {
