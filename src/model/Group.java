17f227ac89ae2fb1518c8b1d9ecc0167dd90a24e
Eisfuchs <Eisfuchs@gmail.com>
2014-10-03 20:08:40 +0200

    #3 Gruppenklasse für die Beipsieloberfläche angelegt

---
+++ src/model/Group.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Group {
+	private String gruppenname;
+	private Boolean befreit = false;
+	
+	public Group(String gruppenname, Boolean befreit) {
+		super();
+		this.gruppenname = gruppenname;
+		this.befreit = befreit;
+	}
+	public String getGruppenname() {
+		return gruppenname;
+	}
+	public void setGruppenname(String gruppenname) {
+		this.gruppenname = gruppenname;
+	}
+	public Boolean getBefreit() {
+		return befreit;
+	}
+	public void setBefreit(Boolean befreit) {
+		this.befreit = befreit;
+	}
+}
