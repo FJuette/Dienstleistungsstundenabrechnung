66d1981d499db495a8433f843baed2b51db909c6
Eisfuchs <Eisfuchs@gmail.com>
2014-10-03 21:16:07 +0200

    #3 Implementierungen für die Oberfläche

---
+++ src/model/Role.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Role {
+	private String rollenname;
+
+	public Role(String rollenname) {
+		super();
+		this.rollenname = rollenname;
+	}
+	
+	public String getRollenname() {
+		return rollenname;
+	}
+
+	public void setRollenname(String rollenname) {
+		this.rollenname = rollenname;
+	}
+}
--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,40 @@
 package model;
 
 public class User {
-
+	private String benutzername;
+	private String passwort;
+	private Boolean aktiv = true;
+	private Role rolle;
+	
+	public User(String benutzername, String passwort, Boolean aktiv, Role rolle) {
+		super();
+		this.benutzername = benutzername;
+		this.passwort = passwort;
+		this.aktiv = aktiv;
+		this.rolle = rolle;
+	}
+	public String getBenutzername() {
+		return benutzername;
+	}
+	public void setBenutzername(String benutzername) {
+		this.benutzername = benutzername;
+	}
+	public String getPasswort() {
+		return passwort;
+	}
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
+	public Boolean getAktiv() {
+		return aktiv;
+	}
+	public void setAktiv(Boolean aktiv) {
+		this.aktiv = aktiv;
+	}
+	public Role getRolle() {
+		return rolle;
+	}
+	public void setRolle(Role rolle) {
+		this.rolle = rolle;
+	}
 }
