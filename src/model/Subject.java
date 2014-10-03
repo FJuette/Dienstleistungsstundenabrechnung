dfc6860290a2386281f5875c28647a445644b003
Eisfuchs <Eisfuchs@gmail.com>
2014-10-03 20:23:08 +0200

    #3 Klasse für die Oberfläche

---
+++ src/model/Subject.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Subject {
+	private String spartenname;
+
+	public String getSpartenname() {
+		return spartenname;
+	}
+
+	public void setSpartenname(String spartenname) {
+		this.spartenname = spartenname;
+	}
+
+	public Subject(String spartenname) {
+		super();
+		this.spartenname = spartenname;
+	}
+	
+	
+}
