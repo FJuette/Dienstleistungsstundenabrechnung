b91940fba596816fd577f8be988c031c941ab1ae
Ei5fuch5 <Eisfuchs@gmail.com>
2014-10-06 15:52:52 +0200

    #9 Layout anpassungen

--- src/Views/GroupsView.java
+++ src/Views/GroupsView.java
@@ -13,32 +13,67 @@ import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 import de.juette.dlsa.BooleanToGermanConverter;
 import de.juette.dlsa.ComponentHelper;
 
 @SuppressWarnings("serial")
-public class GroupsView extends HorizontalSplitPanel implements View {
+public class GroupsView extends VerticalLayout implements View {
 
-	private final Table tblGroups = new Table();
 	@PropertyId("gruppenname")
 	private TextField txtGruppenname = new TextField("Gruppenname:");
 	@PropertyId("befreit")
 	private CheckBox cbBefreit = new CheckBox("Befreit");
-	private FieldGroup fieldGroup;
+
+	private final Table tblGroups = new Table();
+	private FormLayout editorLayout = new FormLayout();
+	private FieldGroup editorFields = new FieldGroup();
 	private BeanItemContainer<Group> groups = new BeanItemContainer<>(Group.class);
 	
+	private final Button btnNewGroup = new Button("Neue Gruppe");
+	private final Button btnSave = new Button("Speichern");
+	
 	public GroupsView() {
-		
+		initLayout();
 		initTable();
-		setFirstComponent(tblGroups);
-		setSecondComponent(createEditFields());
+		initEditor();
+	}
+	
+	private void initLayout() {
+		HorizontalSplitPanel splitPanel = new HorizontalSplitPanel();
+		addComponent(splitPanel);
 		
-		setSplitPosition(15, Unit.PERCENTAGE);
-		setLocked(true);
+		Label title = new Label("Gruppenverwaltung");
+		title.addStyleName("h1");
+		addComponent(title);
+		
+		VerticalLayout leftLayout = new VerticalLayout();
+		setSpacing(true);
+		setMargin(true);
+		addComponent(leftLayout);
+		addComponent(editorLayout);
+		leftLayout.addComponent(tblGroups);
+		
+		splitPanel.setSplitPosition(25, Unit.PERCENTAGE);
+		splitPanel.setLocked(true);
+		
+		HorizontalLayout bottomLeftLayout = new HorizontalLayout();
+		leftLayout.addComponent(bottomLeftLayout);
+		bottomLeftLayout.addComponent(btnNewGroup);
+		
+		leftLayout.setWidth("100%");
+		
+		leftLayout.setExpandRatio(tblGroups, 1);
+		tblGroups.setSizeFull();
+		
+		bottomLeftLayout.setWidth("100%");
+		editorLayout.setMargin(true);
+		editorLayout.setVisible(false);
 	}
 	
 	private void initTable() {
@@ -46,42 +81,46 @@ public class GroupsView extends HorizontalSplitPanel implements View {
 		
 		tblGroups.setContainerDataSource(groups);
 		tblGroups.setSelectable(true);
+		tblGroups.setImmediate(true);
 		tblGroups.setVisibleColumns( new Object[] {"gruppenname", "befreit"} );
 		tblGroups.setColumnHeaders("Gruppe", "Befreit");
-		tblGroups.setHeight("400");
 		tblGroups.setConverter("befreit", new BooleanToGermanConverter());
 		
 		tblGroups.addItemClickListener(event -> {
-			fieldGroup.setItemDataSource(event.getItem());
-			fieldGroup.bindMemberFields(this);
+			editorFields.setItemDataSource(event.getItem());
+			editorFields.bindMemberFields(this);
+			editorLayout.setVisible(event.getItem() != null);
 		});
+		
+		updateTable();
 	}
 	
-	private FormLayout createEditFields() {
-		// Set the Caption of the TextField on the Top
-		HorizontalLayout txtLayout = new HorizontalLayout();
-		txtLayout.addComponent(txtGruppenname);
+	private void initEditor() {
+		editorLayout.addComponent(txtGruppenname);
+		editorLayout.addComponent(cbBefreit);
+		editorLayout.addComponent(btnSave);
+		editorLayout.setWidth("100%");
 		
-		FormLayout layout = new FormLayout();
-		layout.setSizeUndefined();
-		layout.setMargin(true);
-		
-		fieldGroup = new BeanFieldGroup<Group>(Group.class);
-		
-		Button btnSave = new Button("Speichern");
 		btnSave.addClickListener(event -> {
 			try {
-				fieldGroup.commit();
+				editorFields.commit();
 				Notification.show("Speichern erfolgreich", Notification.Type.TRAY_NOTIFICATION);
 			} catch (Exception e) {
 				Notification.show("Fehler: " + e.getMessage(), Notification.Type.ERROR_MESSAGE);
 			}
 		});
 		
-		layout.addComponents(txtLayout, cbBefreit, btnSave);
-		return layout;
+		editorFields.setBuffered(false);
 	}
-
+	
+	private void updateTable() {
+		if (tblGroups.size() > 15) {
+			tblGroups.setPageLength(15);
+		} else {
+			tblGroups.setPageLength(tblGroups.size());
+		}
+		tblGroups.markAsDirtyRecursive();
+	}
 
 	@Override
 	public void enter(ViewChangeEvent event) {
