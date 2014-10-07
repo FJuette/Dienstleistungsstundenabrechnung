9e004de50e5256bafbe614474da13d79cbed61a5
Ei5fuch5 <Eisfuchs@gmail.com>
2014-10-07 17:01:31 +0200

    #8 Ersten Teil angelegt

--- src/Views/BookingView.java
+++ src/Views/BookingView.java
@@ -1,16 +1,43 @@
 package Views;
 
+import model.Booking;
+
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
 
+@SuppressWarnings("serial")
 public class BookingView extends VerticalLayout implements View {
+
+	private final Table tblBookings = new Table();
+	
+	private BeanItemContainer<Booking> bookings = new BeanItemContainer<>(Booking.class);
+		
+	public BookingView() {
+		initLayout();
+		initTable();
+	}
+
+	private void initLayout() {
+		setSpacing(true);
+		setMargin(true);
+		
+		Label title = new Label("Journal");
+		title.addStyleName("h1");
+		addComponent(title);
+		
+		addComponent(tblBookings);
+	}
+	
+	private void initTable() {
+		
+	}
 
 	@Override
 	public void enter(ViewChangeEvent event) {
-		addComponent(new Label("Buchungsverwaltung"));
-
 	}
 
 }
