8b4d412e4254ca7c0db9a58a92c2030178004eab
Eisfuchs <Eisfuchs@gmail.com>
2014-10-09 21:31:32 +0200

    #9 Bearbeitungsmöglichkeiten angepasst

--- src/Views/GroupsView.java
+++ src/Views/GroupsView.java
@@ -2,16 +2,15 @@ package Views;
 
 import model.Group;
 
-import com.vaadin.data.fieldgroup.FieldGroup;
-import com.vaadin.data.fieldgroup.PropertyId;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.Action;
+import com.vaadin.event.Action.Handler;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
@@ -25,24 +24,34 @@ import de.juette.dlsa.ComponentHelper;
 @SuppressWarnings("serial")
 public class GroupsView extends VerticalLayout implements View {
 
-	@PropertyId("gruppenname")
-	private TextField txtGruppenname = new TextField("Gruppenname:");
-	@PropertyId("befreit")
-	private CheckBox cbBefreit = new CheckBox("Befreit");
-
 	private final Table tblGroups = new Table();
-	private FormLayout editorLayout = new FormLayout();
-	private FieldGroup editorFields = new FieldGroup();
 	private BeanItemContainer<Group> groups = new BeanItemContainer<>(Group.class);
 	
 	private final Button btnNewGroup = new Button("Neue Gruppe");
-	private final Button btnSave = new Button("Speichern");
-	private final Button btnDelete = new Button("Löschen");
+	private final Button btnChange = new Button("Bearbeiten");
+	
+	private Handler actionHandler = new Handler() {
+		private final Action REMOVE = new Action("Entfernen");
+		private final Action[] ACTIONS = new Action[] { REMOVE };
+
+		@Override
+		public void handleAction(final Action action, final Object sender,
+				final Object target) {
+			if (action.getCaption().equals("Entfernen")) {
+				groups.removeItem(tblGroups.getValue());
+				updateTable();
+			}
+		}
+
+		@Override
+		public Action[] getActions(final Object target, final Object sender) {
+			return ACTIONS;
+		}
+	};
 	
 	public GroupsView() {
 		initLayout();
 		initTable();
-		initEditor();
 	}
 	
 	private void initLayout() {
@@ -52,34 +61,36 @@ public class GroupsView extends VerticalLayout implements View {
 		Label title = new Label("Gruppenverwaltung");
 		title.addStyleName("h1");
 		addComponent(title);
+				
+		addComponent(tblGroups);
 		
-		HorizontalSplitPanel splitPanel = new HorizontalSplitPanel();
-		addComponent(splitPanel);
-		
-		VerticalLayout leftLayout = new VerticalLayout();
-		splitPanel.addComponent(leftLayout);
-		splitPanel.addComponent(editorLayout);
-		leftLayout.addComponent(tblGroups);
-		
-		splitPanel.setSplitPosition(25, Unit.PERCENTAGE);
-		splitPanel.setLocked(true);
 		
 		HorizontalLayout bottomLeftLayout = new HorizontalLayout();
-		leftLayout.addComponent(bottomLeftLayout);
-		bottomLeftLayout.addComponent(btnNewGroup);
+		bottomLeftLayout.setSpacing(true);
+		addComponent(bottomLeftLayout);
+		bottomLeftLayout.addComponent(btnChange);
 		
+		btnChange.addClickListener(event -> {
+			try {
+				if("Bearbeiten".equals(btnChange.getCaption())) {
+					tblGroups.setEditable(true);
+					btnChange.setCaption("Speichern");
+					updateTable();
+				} else {
+					tblGroups.setEditable(false);
+					btnChange.setCaption("Bearbeiten");
+					Notification.show("Speichern erfolgreich", Notification.Type.TRAY_NOTIFICATION);
+					updateTable();
+				}
+			} catch (Exception e) {
+				Notification.show("Fehler: " + e.getMessage(), Notification.Type.ERROR_MESSAGE);
+			}
+		});
+		
+		bottomLeftLayout.addComponent(btnNewGroup);
 		btnNewGroup.addClickListener(event -> {
 			newGroupWindow();
 		});
-		
-		leftLayout.setWidth("100%");
-		leftLayout.setSpacing(true);
-		leftLayout.setExpandRatio(tblGroups, 1);
-		tblGroups.setSizeFull();
-		
-		bottomLeftLayout.setWidth("100%");
-		editorLayout.setMargin(true);
-		editorLayout.setVisible(false);
 	}
 	
 	private void initTable() {
@@ -92,54 +103,17 @@ public class GroupsView extends VerticalLayout implements View {
 		tblGroups.setVisibleColumns( new Object[] {"gruppenname", "befreit"} );
 		tblGroups.setColumnHeaders("Gruppe", "DLS Befreit");
 		tblGroups.setConverter("befreit", new BooleanToGermanConverter());
-		
-		tblGroups.addItemClickListener(event -> {
-			editorFields.setItemDataSource(event.getItem());
-			editorFields.bindMemberFields(this);
-			editorLayout.setVisible(event.getItem() != null);
-		});
-		
+		tblGroups.addActionHandler(getActionHandler());
+		tblGroups.setWidth("40%");
+
 		updateTable();
-	}
-	
-	private void initEditor() {
-		editorLayout.addComponent(txtGruppenname);
-		txtGruppenname.setWidth("25%");
-		editorLayout.addComponent(cbBefreit);
-		HorizontalLayout buttonLayout = new HorizontalLayout();
-		buttonLayout.setSpacing(true);
-		buttonLayout.addComponents(btnSave, btnDelete);
-		editorLayout.addComponent(buttonLayout);
-		editorLayout.setWidth("100%");
-				
-		btnSave.addClickListener(event -> {
-			try {
-				editorFields.commit();
-				Notification.show("Speichern erfolgreich", Notification.Type.TRAY_NOTIFICATION);
-			} catch (Exception e) {
-				Notification.show("Fehler: " + e.getMessage(), Notification.Type.ERROR_MESSAGE);
-			}
-		});
-		
-		btnDelete.setStyleName("danger");
-		btnDelete.addClickListener(event -> {
-			try {
-				groups.removeItem(tblGroups.getValue());
-				updateTable();
-				editorLayout.setVisible(false);
-			} catch (Exception e2) {
-				Notification.show("Fehler: " + e2.getMessage(), Notification.Type.ERROR_MESSAGE);
-			}
-		});
-		
-		editorFields.setBuffered(false);
 	}
 	
 	private void updateTable() {
 		if (tblGroups.size() > 15) {
 			tblGroups.setPageLength(15);
 		} else {
-			tblGroups.setPageLength(tblGroups.size());
+			tblGroups.setPageLength(tblGroups.size() + 1);
 		}
 		tblGroups.markAsDirtyRecursive();
 	}
@@ -170,6 +144,10 @@ public class GroupsView extends VerticalLayout implements View {
 		});
 		
 		getUI().addWindow(window);
+	}
+	
+	private Handler getActionHandler() {
+		return actionHandler;
 	}
 
 	@Override
