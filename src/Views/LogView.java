75ccd81819869c95683a886823156702f52a48a1
Eisfuchs <Eisfuchs@gmail.com>
2014-10-14 22:20:45 +0200

    #12 Ein Beispiel für einen alten Jahreslauf lässt sich herunterladen Alte Jahresläufe werden angezeigt

--- src/Views/LogView.java
+++ src/Views/LogView.java
@@ -1,17 +1,113 @@
 package Views;
 
+import java.io.File;
+
+import model.Cycle;
+import model.Log;
+
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.Action;
+import com.vaadin.event.Action.Handler;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.FileDownloader;
+import com.vaadin.server.FileResource;
+import com.vaadin.server.Resource;
+import com.vaadin.server.VaadinService;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
+
+import de.juette.dlsa.ComponentHelper;
 
 @SuppressWarnings("serial")
 public class LogView extends VerticalLayout implements View {
 
-	@Override
-	public void enter(ViewChangeEvent event) {
-		addComponent(new Label("Historie"));
+	private Handler actionHandler = new Handler() {
+		private final Action REVERT = new Action("Rückgängig");
+		private final Action[] ACTIONS = new Action[] { REVERT };
 
+		@Override
+		public void handleAction(final Action action, final Object sender,
+				final Object target) {
+			if (action.getCaption().equals("Rückgängig")) {
+				Notification.show("Änderung erfolgreich Rückgängig gemacht");
+			}
+		}
+
+		@Override
+		public Action[] getActions(final Object target, final Object sender) {
+			return ACTIONS;
+		}
+	};
+
+	private final Table tblLog = new Table();
+	private BeanItemContainer<Log> logEntrys = new BeanItemContainer<>(
+			Log.class);
+	private final ComboBox oldCycles = new ComboBox(
+			"Auswertungen der vergangenen Jahresläufe");
+	private BeanItemContainer<Cycle> cycles = new BeanItemContainer<>(
+			Cycle.class);
+
+	public LogView() {
+		initLayout();
+		initTable();
 	}
 
+	private void initLayout() {
+		setSpacing(true);
+		setMargin(true);
+
+		Label title = new Label("Historie");
+		title.addStyleName("h1");
+		addComponent(title);
+
+		cycles = ComponentHelper.getDummyCycles();
+
+		oldCycles.setContainerDataSource(cycles);
+		oldCycles.setItemCaptionPropertyId("anzeigename");
+		addComponent(oldCycles);
+
+		Button saveFile = new Button("Download");
+		addComponent(saveFile);
+
+		oldCycles.addValueChangeListener(event -> {
+			String basepath = VaadinService.getCurrent().getBaseDirectory()
+					.getAbsolutePath();
+			Resource res = new FileResource(new File(basepath
+					+ "/WEB-INF/Files/ExampleResult.csv"));
+			FileDownloader fd = new FileDownloader(res);
+			fd.extend(saveFile);
+		});
+
+		addComponent(tblLog);
+	}
+
+	private void initTable() {
+		logEntrys = ComponentHelper.getDummyLog();
+		logEntrys.addNestedContainerProperty("bearbeiter.fullName");
+
+		tblLog.setContainerDataSource(logEntrys);
+		tblLog.setSelectable(true);
+		tblLog.setImmediate(true);
+		tblLog.setRowHeaderMode(Table.RowHeaderMode.INDEX);
+		tblLog.setVisibleColumns(new Object[] { "timestamp", "beschreibung",
+				"bearbeiter.fullName" });
+		tblLog.setColumnHeaders("Zeitpunkt", "Beschreibung", "Bearbeiter");
+		tblLog.setWidth("80%");
+		tblLog.addActionHandler(getActionHandler());
+
+		ComponentHelper.updateTable(tblLog);
+	}
+
+	private Handler getActionHandler() {
+		return actionHandler;
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+	}
 }
--- src/de/juette/dlsa/ComponentHelper.java
+++ src/de/juette/dlsa/ComponentHelper.java
@@ -1,31 +1,36 @@
 package de.juette.dlsa;
 
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import javafx.scene.shape.Cylinder;
 import model.Activity;
 import model.Booking;
+import model.Cycle;
 import model.Group;
+import model.Log;
 import model.Member;
 import model.Role;
 import model.Subject;
 import model.User;
 
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.VaadinService;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
 
 public class ComponentHelper {
-	
+
 	// Set the Caption of the TextField on the Top
 	public static HorizontalLayout getCaptionOnTop(Component c) {
 		HorizontalLayout txtLayout = new HorizontalLayout();
 		txtLayout.addComponent(c);
 		return txtLayout;
 	}
-	
+
 	public static void updateTable(Table table) {
 		if (table.size() > 15) {
 			table.setPageLength(15);
@@ -34,18 +39,20 @@ public class ComponentHelper {
 		}
 		table.markAsDirtyRecursive();
 	}
-	
+
 	public static BeanItemContainer<Subject> getDummySubjects() {
-		BeanItemContainer<Subject> subjects = new BeanItemContainer<Subject>(Subject.class);
+		BeanItemContainer<Subject> subjects = new BeanItemContainer<Subject>(
+				Subject.class);
 		subjects.addItem(new Subject("Spasstänzer"));
 		subjects.addItem(new Subject("Standdardstänzer"));
 		subjects.addItem(new Subject("Rutinetänzer"));
 		subjects.addItem(new Subject("Proditänzer"));
 		return subjects;
 	}
-	
+
 	public static BeanItemContainer<Group> getDummyGroups() {
-		BeanItemContainer<Group> groups = new BeanItemContainer<Group>(Group.class);
+		BeanItemContainer<Group> groups = new BeanItemContainer<Group>(
+				Group.class);
 		groups.addItem(new Group("Trainer", true));
 		groups.addItem(new Group("Vorstand", true));
 		groups.addItem(new Group("Mitglied", false));
@@ -54,80 +61,125 @@ public class ComponentHelper {
 	}
 
 	public static BeanItemContainer<Member> getDummyMembers() {
-		BeanItemContainer<Member> members = new BeanItemContainer<Member>(Member.class);
-		
+		BeanItemContainer<Member> members = new BeanItemContainer<Member>(
+				Member.class);
+
 		ArrayList<Group> sGroups = new ArrayList<Group>();
 		sGroups.add(getDummyGroups().getIdByIndex(0));
 		sGroups.add(getDummyGroups().getIdByIndex(1));
-		
+
 		ArrayList<Subject> sSubjects = new ArrayList<Subject>();
 		sSubjects.add(getDummySubjects().getIdByIndex(2));
 		sSubjects.add(getDummySubjects().getIdByIndex(3));
-		
+
 		ArrayList<Group> jGroups = new ArrayList<Group>();
 		jGroups.add(getDummyGroups().getIdByIndex(2));
 		jGroups.add(getDummyGroups().getIdByIndex(3));
-		
+
 		ArrayList<Subject> jSubjects = new ArrayList<Subject>();
 		jSubjects.add(getDummySubjects().getIdByIndex(0));
 		jSubjects.add(getDummySubjects().getIdByIndex(1));
-		
-		members.addItem(new Member("Sander", "Thorsten", "123456", sGroups, sSubjects));
-		members.addItem(new Member("Juette", "Fabian", "987654", jGroups, jSubjects));
-		members.addItem(new Member("Tester", "Tom", "565645", sGroups, sSubjects));
-		members.addItem(new Member("Juppie", "Jörn", "848484", jGroups, jSubjects));
+
+		members.addItem(new Member("Sander", "Thorsten", "123456", sGroups,
+				sSubjects));
+		members.addItem(new Member("Juette", "Fabian", "987654", jGroups,
+				jSubjects));
+		members.addItem(new Member("Tester", "Tom", "565645", sGroups,
+				sSubjects));
+		members.addItem(new Member("Juppie", "Jörn", "848484", jGroups,
+				jSubjects));
 
 		return members;
 	}
-		
+
 	public static BeanItemContainer<Activity> getDummyActivities() {
-		BeanItemContainer<Activity> activities = new BeanItemContainer<>(Activity.class);
-		activities.addItem(new Activity(
-				"2014", "Erste Aktion", 
-				5, getDummyMembers().getIdByIndex(1), 
-				getDummyMembers().getIdByIndex(2)));
-		activities.addItem(new Activity(
-				"2014", "Zweite Aktion", 
-				(float) 3.5, getDummyMembers().getIdByIndex(2), 
-				getDummyMembers().getIdByIndex(3)));
-		activities.addItem(new Activity(
-				"2014", "Dritte Aktion", 
-				(float) 2.5, getDummyMembers().getIdByIndex(3), 
-				getDummyMembers().getIdByIndex(1)));
+		BeanItemContainer<Activity> activities = new BeanItemContainer<>(
+				Activity.class);
+		activities.addItem(new Activity("2014", "Erste Aktion", 5,
+				getDummyMembers().getIdByIndex(1), getDummyMembers()
+						.getIdByIndex(2)));
+		activities.addItem(new Activity("2014", "Zweite Aktion", (float) 3.5,
+				getDummyMembers().getIdByIndex(2), getDummyMembers()
+						.getIdByIndex(3)));
+		activities.addItem(new Activity("2014", "Dritte Aktion", (float) 2.5,
+				getDummyMembers().getIdByIndex(3), getDummyMembers()
+						.getIdByIndex(1)));
 		return activities;
 	}
-	
+
 	public static BeanItemContainer<Booking> getDummyBookings() {
-		BeanItemContainer<Booking> bookings = new BeanItemContainer<Booking>(Booking.class);
+		BeanItemContainer<Booking> bookings = new BeanItemContainer<Booking>(
+				Booking.class);
 		try {
-			bookings.addItem(new Booking(
-					2, "Erste Buchung", 
-					new SimpleDateFormat("dd.MM.yyyy").parse("01.10.2014"), getDummyMembers().getIdByIndex(3), 
-					getDummyActivities().getIdByIndex(1), getDummyMembers().getIdByIndex(2)));
-			bookings.addItem(new Booking(
-					3, "Zweite Buchung", 
-					new SimpleDateFormat("dd.MM.yyyy").parse("11.01.2014"), getDummyMembers().getIdByIndex(2), 
-					getDummyActivities().getIdByIndex(2), getDummyMembers().getIdByIndex(1)));
+			bookings.addItem(new Booking(2, "Erste Buchung",
+					new SimpleDateFormat("dd.MM.yyyy").parse("01.10.2014"),
+					getDummyMembers().getIdByIndex(3), getDummyActivities()
+							.getIdByIndex(1), getDummyMembers().getIdByIndex(2)));
+			bookings.addItem(new Booking(3, "Zweite Buchung",
+					new SimpleDateFormat("dd.MM.yyyy").parse("11.01.2014"),
+					getDummyMembers().getIdByIndex(2), getDummyActivities()
+							.getIdByIndex(2), getDummyMembers().getIdByIndex(1)));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return bookings;
 	}
-	
+
 	public static BeanItemContainer<User> getDummyUsers() {
 		BeanItemContainer<User> users = new BeanItemContainer<User>(User.class);
-		users.addItem(new User("Administrator", "geheim", true, getDummyRoles().getIdByIndex(0)));
-		users.addItem(new User("Benutzer1", "nochgeheimer", true, getDummyRoles().getIdByIndex(1)));
-		users.addItem(new User("Inaktiver", "geheim", false, getDummyRoles().getIdByIndex(1)));
+		users.addItem(new User("Administrator", "geheim", true, getDummyRoles()
+				.getIdByIndex(0)));
+		users.addItem(new User("Benutzer1", "nochgeheimer", true,
+				getDummyRoles().getIdByIndex(1)));
+		users.addItem(new User("Inaktiver", "geheim", false, getDummyRoles()
+				.getIdByIndex(1)));
 		return users;
 	}
-	
+
 	public static BeanItemContainer<Role> getDummyRoles() {
 		BeanItemContainer<Role> roles = new BeanItemContainer<Role>(Role.class);
 		roles.addItem(new Role("Admin"));
 		roles.addItem(new Role("Benutzer"));
 		return roles;
 	}
-	
+
+	public static BeanItemContainer<Log> getDummyLog() {
+		BeanItemContainer<Log> logEntrys = new BeanItemContainer<Log>(Log.class);
+		try {
+			logEntrys
+					.addItem(new Log(
+							new SimpleDateFormat("dd.MM.yyyy hh:mm")
+									.parse("13.03.2014 10:23"),
+							"Benutzer Juette bearbeitet, veränderte(s) Feld(er): Nachname",
+							getDummyMembers().getIdByIndex(1)));
+			logEntrys
+					.addItem(new Log(
+							new SimpleDateFormat("dd.MM.yyyy hh:mm")
+									.parse("14.03.2014 17:53"),
+							"Für das Mitglied Tom Tester sind für die Aktion (Erste Aktion) 4 Dienstleistungssunden verbucht",
+							getDummyMembers().getIdByIndex(0)));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return logEntrys;
+	}
+
+	public static BeanItemContainer<Cycle> getDummyCycles() {
+		BeanItemContainer<Cycle> cycles = new BeanItemContainer<Cycle>(
+				Cycle.class);
+		File file = new File(VaadinService.getCurrent().getBaseDirectory()
+				.getAbsolutePath()
+				+ "/WEB-INF/Files/ExampleResult.csv");
+		try {
+			cycles.addItem(new Cycle(file, new SimpleDateFormat(
+					"dd.MM.yyyy hh:mm").parse("13.03.2014 10:23"),
+					"Jahreslauf vom 31.03.2013"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cycles;
+	}
 }
